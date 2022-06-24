c66d0bfdedf6e642e31e53d44517aac0ae25554c
Marko_TON <markotoncanovic@gmail.com>
2022-06-24 16:21:50 +0200

    add some random text under UL

--- contextapp/src/components/BookList.js
+++ contextapp/src/components/BookList.js
@@ -8,6 +8,7 @@ class BookList extends Component {
           <li>the way of kings</li>
           <li>the name of the wind</li>
           <li>the final empire</li>
+          <li>Lorem ipsum dolor sit amet.</li>
         </ul>
       </div>
     );
